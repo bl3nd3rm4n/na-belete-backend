a77b3a1decd2c8b9707eadb4634bb87257036d33
bl3nd3rm4n <ionut.ispas@yahoo.com>
2020-08-11 20:05:55 +0300

    first commit

---
+++ src/main/java/com/blndr/nabeletebackend/NaBeleteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.blndr.nabeletebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaBeleteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaBeleteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blndr/nabeletebackend/NaBeleteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blndr.nabeletebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaBeleteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
