12ba9f52f0c4a02ccebe537ad181f93ddd8418b2
bl3nd3rm4n <ionut.ispas@yahoo.com>
2020-08-15 14:48:05 +0300

    authentication and role based authorization working

---
+++ src/main/java/com/blndr/nabeletebackend/controller/Controller.java
@@ -0,0 +1,56 @@
+package com.blndr.nabeletebackend.controller;
+
+import com.blndr.nabeletebackend.model.Holders.AuthenticationRequest;
+import com.blndr.nabeletebackend.model.Holders.AuthenticationResponse;
+import com.blndr.nabeletebackend.services.UserService;
+import com.blndr.nabeletebackend.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @GetMapping("/")
+    public String home() {
+        return ("<h1>Welcome</h1>");
+    }
+
+    @GetMapping("/user")
+    public String user(Authentication authentication) {
+
+        return ("<h1>Welcome User " + authentication.getName() + " " + authentication.getCredentials() + "</h1>");
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return ("<h1>Welcome Admin</h1>");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new Exception("Incorrect username or password", e);
+        }
+        final UserDetails userDetails = userService.loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthenticationResponse(jwt));
+    }
+}
---
+++ src/main/java/com/blndr/nabeletebackend/services/UserService.java
@@ -0,0 +1,26 @@
+package com.blndr.nabeletebackend.services;
+
+import com.blndr.nabeletebackend.model.User;
+import com.blndr.nabeletebackend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@Primary
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findUserByEmail(username);
+        user.orElseThrow(() -> new UsernameNotFoundException(username + " not found."));
+        return user.get();
+    }
+}
